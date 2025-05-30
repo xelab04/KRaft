f1c4b300e48b37fb9fa523695fae376c74fcb207
xelab04 <alexbissessur@gmail.com>
2025-05-30 23:02:42 +0400

    get resource used by pods in namespace

--- resources-manage/main.py
+++ resources-manage/main.py
@@ -3,11 +3,22 @@ from kubernetes import client, config
 def convert_cpu(cpu):
     if cpu.endswith("m"):
         cpu = int(cpu.strip("m"))
-    elif cpu.endswith("cpu"):
-        cpu = int(cpu)
     else:
-        raise ValueError("Invalid CPU value")
+        try:
+            cpu = int(cpu) * 1000
+        except ValueError:
+            raise ValueError("Invalid CPU value")
     return cpu
+
+def convert_memory(mem):
+    if mem.endswith("Mi"):
+        mem = int(mem.strip("Mi"))
+    elif mem.endswith("Gi"):
+        # multiplying by 1024 is ugly
+        mem = int(mem.strip("Gi")) * 1024
+    else:
+        raise ValueError("Invalid memory value")
+    return mem
 
 def main():
     config.load_kube_config()
@@ -16,8 +27,15 @@ def main():
     ret = api_instance.list_namespaced_pod(namespace="caddy", watch=False)
     for pod in ret.items:
         for container in pod.spec.containers:
-            print(container.resources.requests['cpu'])
-            print(container.resources.requests['memory'])
+            cpu, memory = 0, 0
+            if(container.resources.requests):
+                if container.resources.requests.get('cpu'):
+                    cpu = convert_cpu(container.resources.requests['cpu'])
+                if container.resources.requests.get('memory'):
+                    memory = convert_memory(container.resources.requests['memory'])
+            print(cpu, memory)
+            # print(container.resources.requests['cpu'])
+            # print(container.resources.requests['memory'])
         print("%s\t%s\t%s" % (pod.status.pod_ip, pod.metadata.namespace, pod.metadata.name))
     print("Done")
 
