7f29fa4f84cec92f9b7b93b91a0deaf404ae363d
xelab04 <alexbissessur@gmail.com>
2025-08-10 16:53:17 +0400

    added database component for testing

---
+++ database/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+    user_id int NOT NULL,
+    -- AUTO_INCREMENT,
+    user_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE clusters (
+    cluster_id int NOT NULL AUTO_INCREMENT,
+    cluster_name varchar(255) NOT NULL,
+    user_id int NOT NULL,
+    PRIMARY KEY (cluster_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+INSERT INTO users VALUES (0, "alex", "alexbissessur@gmail.com", "password");
