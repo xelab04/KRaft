b15b9aaca4430ed3a4f9e546da78995fafdab35e
xelab04 <alexbissessur@gmail.com>
2025-10-24 22:56:08 +0400

    ft: add ingress create for cluster

---
+++ cluster-manage/src/ingress.rs
@@ -0,0 +1,48 @@
+use kube::{
+    api::{Api, PostParams, ResourceExt},
+    core::{DynamicObject, GroupVersionKind, ApiResource},
+    Client,
+};
+use serde_json::json;
+
+pub async fn traefik(client: &Client, cluster_name: &String, namespace: &String, host: &str) -> bool {
+
+    // define CRD type
+    let gvk = GroupVersionKind::gvk("traefik.io", "v1alpha1", "IngressRouteTCP");
+    let ar = ApiResource::from_gvk(&gvk);
+
+    // api
+    let ingress_routes: Api<DynamicObject> = Api::namespaced_with(client.clone(), namespace.as_str(), &ar);
+
+    // json cause im lazy
+    let ingressroute = json!({
+        "apiVersion": "traefik.io/v1alpha1",
+        "kind": "IngressRouteTCP",
+        "metadata": {
+            "name": format!("api-svr-{}",cluster_name),
+            "namespace": namespace
+        },
+        "spec": {
+            "entryPoints": ["websecure"],
+            "routes": [
+                {
+                    "match": format!("HostSNI(`{}`)", host),
+                    "services": [
+                        {
+                            "name": format!("k3k-{}-service",cluster_name),
+                            "port": 443
+                        }
+                    ]
+                }
+            ],
+            "tls": { "passthrough": true }
+        }
+    });
+
+    let pp = PostParams::default();
+    let ingressroute: DynamicObject = serde_json::from_value(ingressroute).unwrap();
+
+    let created = ingress_routes.create(&pp, &ingressroute).await.unwrap();
+
+    true
+}
