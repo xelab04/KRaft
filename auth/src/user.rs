2880f20e64ab3c621a790ab367b8809067cb6d27
alex <alexbissessur@gmail.com>
2025-12-09 13:24:32 +0400

    add uuid to user detail fetch

--- auth/src/user.rs
+++ auth/src/user.rs
@@ -11,51 +11,47 @@ use crate::auth;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, FromRow, Clone)]
 struct User {
-    user_id: Option<i32>,
-    username: Option<String>,
+    user_id: i32,
+    username: String,
     email: String,
+    uuid: String,
     #[serde(rename = "password")]
+    #[sqlx(skip)]
     user_password: String,
     #[sqlx(skip)]
     betacode: Option<String>
 }
 
+#[derive(serde::Serialize, serde::Deserialize)]
+struct UserUUID {
+    u: String
+}
 
 #[actix_web::get("/auth/user/details")]
 pub async fn details(
     req: HttpRequest,
     pool: web::Data<MySqlPool>,
-    user: auth::AuthUser
+    user: auth::AuthUser,
+    user_uuid: web::Query<UserUUID>
 ) -> HttpResponse {
 
-
     let user_id = user.user_id;
-
-    // get user id from request
-    // let jwt = jwt::extract_user_id_from_jwt(&req);
-
-    // let mut user_id: String = String::from("0");
-    // match jwt {
-    //     Ok(id) => {
-    //         user_id = Some(id).unwrap();
-    //     }
-    //     Err(e) => {
-    //         println!("Error: {:?}", e);
-    //         return HttpResponse::Unauthorized().json(json!({"status": "error", "message": "Unauthorized"}));
-    //     }
-    // };
-
-
+    let user_uuid = &user_uuid.u;
 
     // get user details from database
-    let user = sqlx::query_as::<_, User>("SELECT user_id, username, email, password as user_password FROM users WHERE user_id = (?)")
-        .bind(user_id)
+    let user = sqlx::query_as::<_, User>("SELECT user_id, username, email, uuid FROM users WHERE uuid = (?)")
+        .bind(user_uuid)
         .fetch_one(pool.as_ref())
         .await;
 
     // return user if valid
     match user {
         Ok(user) => {
+            // Check that the user who made the request matches the uuid fetched
+            if user_id != user.user_id.to_string() {
+                return HttpResponse::Unauthorized().json(json!({"status": "error", "message": "Unauthorized"}));
+            }
+
             HttpResponse::Ok().json(json!({"status": "success", "data": user}))
         }
         Err(e) => {
