91323bf56d322d9389a9263ec866f415cbb05220
xelab04 <alexbissessur@gmail.com>
2025-12-28 20:22:24 +0400

    simplify name validation

--- cluster-manage/src/validatename.rs
+++ cluster-manage/src/validatename.rs
@@ -1,11 +1,23 @@
 pub fn namevalid(name: &String) -> bool {
-    for ch in name.chars() {
-        let c_string = ch.to_string();
-        let c: &str = c_string.as_str();
 
-        if !((c >= "a" && c <= "z") || (c >= "0" && c <= "9") || c == "-") {
-            return false;
-        }
-    }
-    return true;
+    return name.chars().all(|ch|
+        (ch >= 'a' && ch <= 'z')
+        || (ch >= '0' && ch <= '9')
+        || ch == '-'
+    );
+
+    // for ch in name.chars() {
+
+    //     if (ch.is_ascii_alphanumeric() && ch.is_ascii_lowercase()) || ch == '-' {
+    //         continue;
+    //     }
+
+    //     // let c_string = ch.to_string();
+    //     // let c: &str = c_string.as_str();
+
+    //     // if !((ch >= 'a' && c <= "z") || (c >= "0" && c <= "9") || c == "-") {
+    //     //     return false;
+    //     // }
+    // }
+    // return true;
 }
