ca756d8d24b271ba2fc4093b5de0b880cadc3823
alex <alexbissessur@gmail.com>
2026-02-02 14:50:45 +0400

    use structs from class file

--- auth/src/auth.rs
+++ auth/src/auth.rs
@@ -1,66 +1,16 @@
-use argon2::{Argon2, PasswordHasher, PasswordVerifier, password_hash::Salt};
-use argon2::{password_hash::{PasswordHash, SaltString}};
+use argon2::{Argon2, PasswordVerifier};
+use argon2::{password_hash::{PasswordHash}};
 use jsonwebtoken::{decode, DecodingKey, Validation, Algorithm};
 
-use actix_web::{web, HttpRequest, HttpResponse, FromRequest, Error, cookie::{Cookie, SameSite, time}};
-use rand;
+use actix_web::{web, HttpRequest, HttpResponse, cookie::{Cookie, SameSite}};
 use uuid;
-
-use serde::{Serialize, Deserialize};
 use serde_json::{self, json};
-use sqlx::{MySqlPool, FromRow};
-
-use futures_util::future::{ready, Ready};
+use sqlx::{MySqlPool};
 
 use crate::jwt;
 use crate::util::{check_passwords_match, hash_password};
+use crate::class::{PasswordChange, User, AuthUser, PasswordParams, Claims};
 
-
-#[derive(serde::Serialize, serde::Deserialize, Debug, FromRow, Clone)]
-struct User {
-    user_id: Option<i32>,
-    username: Option<String>,
-    uuid: Option<String>,
-    email: String,
-    #[serde(rename = "password")]
-    user_password: String,
-    betacode: Option<String>
-}
-
-pub struct AuthUser {
-    pub user_id: String
-}
-
-impl FromRequest for AuthUser {
-    type Error = Error;
-    type Future = Ready<Result<Self, Self::Error>>;
-
-    fn from_request(req: &HttpRequest, _payload: &mut actix_web::dev::Payload) -> Self::Future {
-        let jwt = jwt::extract_user_id_from_jwt(&req);
-        match jwt {
-            Ok(id) => { return ready(Ok(AuthUser { user_id: id })); }
-            Err(e) => { return ready(Err(actix_web::error::ErrorUnauthorized("Unauthorised"))); }
-        };
-    }
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Debug, FromRow, Clone)]
-struct PasswordChange {
-    current_password: String,
-    new_password: String
-}
-
-#[derive(Deserialize)]
-struct PasswordParams{
-    user_password: String
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Claims {
-    pub sub: String,
-    pub exp: usize,
-    pub iat: usize,
-}
 
 #[actix_web::get("/auth/password")]
 pub async fn password(query: web::Query<PasswordParams>) -> HttpResponse {
@@ -74,8 +24,6 @@ pub async fn password(query: web::Query<PasswordParams>) -> HttpResponse {
         }
     ))
 }
-
-
 
 #[actix_web::post("/auth/changepassword")]
 pub async fn changepwd(
