82dbdbd2431af17cd069c78699fac28931e181ef
alex <alexbissessur@gmail.com>
2026-05-25 10:21:57 +0400

    start resources in core rewrite

--- kraft-core/src/Controllers/mod.rs
+++ kraft-core/src/Controllers/mod.rs
@@ -3,6 +3,7 @@ pub mod ClusterController;
 pub mod DBHelper;
 pub mod JWTController;
 pub mod LogsController;
+pub mod ResourceController;
 pub mod UserController;
 pub mod WorkspaceController;
 pub mod utils;
