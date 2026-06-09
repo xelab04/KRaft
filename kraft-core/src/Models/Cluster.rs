6a7efd7c7b66c57488746d70947c8a4428cd1f75
alex <alexbissessur@gmail.com>
2026-06-09 11:51:50 +0400

    feat: add separate config for cluster resources and limits

--- kraft-core/src/Models/Cluster.rs
+++ kraft-core/src/Models/Cluster.rs
@@ -1,5 +1,6 @@
 use serde::{self, Deserialize, Serialize};
 use sqlx::FromRow;
+use k8s_openapi::apimachinery::pkg::util::intstr::IntOrString;
 
 #[derive(Serialize, Deserialize, FromRow)]
 pub struct Cluster {
@@ -13,4 +14,28 @@ pub struct ClusterCreateForm {
     pub id: Option<i64>,
     pub name: String,
     pub tlssan_array: Option<Vec<String>>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ClusterResourceConfig {
+    pub cluster_resources: ClusterResources,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ClusterResources {
+    pub servers: ResourceCategory,
+    pub workers: ResourceCategory,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ResourceCategory {
+    pub requests: ResourceValues,
+    pub limits: ResourceValues,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ResourceValues {
+    pub cpu: IntOrString,
+    pub memory: IntOrString,
 }
